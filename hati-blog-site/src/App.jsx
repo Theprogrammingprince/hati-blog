013cc7e5fe9fa630f33058bda0ce1c3c6808c2a3
Techbishop <you@example.com>
2024-02-27 07:27:09 +0100

    created the pages and route

--- hati-blog-site/src/App.jsx
+++ hati-blog-site/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Project from './pages/Project'
 
 export default function App() {
   return (
-    <div>App</div>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/project" element={<Project />} />
+    </Routes>
+   </BrowserRouter>
   )
 }
